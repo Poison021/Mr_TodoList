fb08f21e066f19ea66f428936754cd5e3a469a32
Mohamadreza <alirezavahidi8295@gmail.com>
2025-08-03 13:09:13 +0330

    resp

--- src/App.jsx
+++ src/App.jsx
@@ -11,16 +11,18 @@ export default function App() {
       >
         <motion.img
           src="./img/axe.png"
-          alt="جنگجو"
+          alt="Warrior"
           initial={{ x: -300, opacity: 0, rotate: 0 }}
-          animate={{ x: -150, opacity: 1, rotate: [0, 2, -3, 0] }}
+          animate={{ x: -160, opacity: 1, rotate: [0, 2, -3, 0] }}
           transition={{ duration: 2, ease: "easeInOut" }}
-          className="hidden md:block w-3xl  h-auto  top-10 left-10 select-none pointer-events-none fixed z-20"
+          className="hidden md:block w-3xl h-auto top-10 left-10 select-none pointer-events-none fixed z-20"
           draggable={false}
         />
 
-        <div className="z-10 fixed top-40">
-          <TodoList />
+        <div className="z-10 fixed top-24 w-full px-4 md:px-0 md:top-40 flex justify-center">
+          <div className="w-full max-w-md">
+            <TodoList />
+          </div>
         </div>
       </div>
     </TodoProvider>
